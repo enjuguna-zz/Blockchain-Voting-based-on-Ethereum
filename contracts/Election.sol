908509d3c08104bf14244575dfbedfc52a070bb4
enjuguna <eric7njuguna@gmail.com>
2020-11-11 21:36:16 +0300

    smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election
+{
+    //Store Candidate
+    //Read Candidate
+    string public candidate;
+    //Constructor
+     constructor() public
+    {
+        candidate = "Candidate1";
+    }
+}
